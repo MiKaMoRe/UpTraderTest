8c91d24531c102f5f8894bd9bf30db771f2ba92c
mikamore <mikamore-inc@gmail.com>
2023-04-17 07:54:32 +0300

    Add validations

--- src/menu/models.py
+++ src/menu/models.py
@@ -1,10 +1,8 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Directory(models.Model):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     is_root = models.BooleanField(null=False, default=False, verbose_name='корневая?')
     name = models.CharField(
         max_length=255,
@@ -17,6 +15,7 @@ class Directory(models.Model):
         verbose_name='родитель',
         on_delete=models.CASCADE,
         related_name='childs',
+        blank=True,
         null=True
     )
 
@@ -26,3 +25,21 @@ class Directory(models.Model):
     class Meta:
         verbose_name = 'директория'
         verbose_name_plural = 'директории'
+    
+    def validate_parent(self):
+        if self.parent.pk == self.pk:
+            raise ValidationError("Directory self parent")
+
+    def validate_root(self):
+        if self.is_root and self.parent != None:
+            raise ValidationError("Root directory can't have a parent")
+    
+    def clean(self):
+        if self.parent == None:
+            self.is_root == True
+        self.validate_parent()
+        self.validate_root()
+    
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
